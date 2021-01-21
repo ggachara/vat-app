c1d250ab99eace76d78f55f020f90319cd695bf0
ggachara <ginarabharca@gmail.com>
2021-01-21 15:38:40 +0300

    this is another supreme commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import  Tasks  from "./components/Tasks";
+import  Header  from "./components/Header";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctors Appointment',
+      day: 'Feb 14 2020',
+      reminder: 'true'
+    },
+    {
+      id: 2,
+      text: 'Nurses Appointment',
+      day: 'Feb 15 2020',
+      reminder: 'true'
+    },
+    {
+      id: 3,
+      text: 'Whole Hospitals Appointment',
+      day: 'Feb 2020',
+      reminder: 'true'
+    },
+  ])
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  const toggleReminder = (id) => {
+      setTasks(
+        tasks.map((task) => 
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+         )
+      )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+     <Header />
+     {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'Show nothing here!!!'}
     </div>
   );
 }
