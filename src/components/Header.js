ad1d12f2659b80ed2544751f02b881de329c3ff2
ggachara <ginarabharca@gmail.com>
2021-01-22 13:39:36 +0300

    readme update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
         <header className="header">
-            <h1>Tracker App</h1>
+            <h1>Progress Tracker</h1>
             <button className="btn">Add</button> 
         </header>
     )
